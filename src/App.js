d9383c893ad27eb81c248f47a501e34bb31722da
dafuentes <darifuentes@gmail.com>
2021-10-30 15:40:08 -0600

    Upload to heroku

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-12 bg-gray-100">
       <div className="max-w-md w-full space-y-8 bg-white shadow overflow-hidden sm:rounded-lg">
         <div className="px-10 py-5">
-          <h1 className="text-2xl font-bold my-4">Lista de Tareas</h1>
+          <h1 className="text-2xl font-bold my-4">Lista de Tareas Calidad</h1>
           <FormTodo handleAddItem={handleAddItem} />
           <TaskList list={list} setList={setList} />
         </div>
